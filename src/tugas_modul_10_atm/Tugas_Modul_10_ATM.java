32921a6fc37e8b5b3ffc6b5a9aaad953f361f267
Rizki <rizkiamrullah38@gmail.com>
2017-02-11 22:10:15 +0700

    Tugas_Modul_10_ATM

---
+++ src/tugas_modul_10_atm/Tugas_Modul_10_ATM.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_modul_10_atm;
+
+/**
+ *
+ * @author MRX
+ */
+public class Tugas_Modul_10_ATM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
